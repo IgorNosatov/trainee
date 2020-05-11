d1ace095c642854efb82222c7438aaf7928a8749
ihor-nosatov <igornosatov@yahoo.com>
2020-05-11 20:23:30 +0300

    components for web pages

---
+++ posts.php
@@ -0,0 +1,63 @@
+<?php
+    require("partials/header.php");
+    ?>
+<section class="main_content">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="posts pt-5 pb-5">
+          <div class="post-info">
+            <h4>Post name: Lorem Ipsum oftware like Aldus</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st <br>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.</p>
+          </div>
+          <div class="post-info">
+            <h4>Post name: Lorem Ipsum oftware like Aldus</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st <br>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.</p>
+          </div>
+          <div class="post-info">
+            <h4>Post name: Lorem Ipsum oftware like Aldus</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st <br>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.</p>
+          </div>
+          <div class="post-info">
+            <h4>Post name: Lorem Ipsum oftware like Aldus</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st <br>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.</p>
+          </div>
+          <div class="post-info">
+            <h4>Post name: Lorem Ipsum oftware like Aldus</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st <br>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's st.</p>
+          </div>
+        </div>
+        <nav aria-label="Page navigation ">
+          <ul class="pagination">
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</section>
+<?php
+    require("partials/footer.php");
+    ?>
---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+    require("partials/header.php");
+    ?>
+
+<section class="main_content">
+  <div class="container">
+    <div class="row justify-content-md-center">
+      <div class="col-md-7">
+        <div class="register_form">
+          <h3 class="p-3">Register Form</h3>
+          <form class="p-3 pt-5 w-50">
+            <div class="form-group">
+              <label for="inputFullName">Full Name</label>
+              <input type="text" class="form-control" id="inputFullName">
+            </div>
+            <div class="form-group">
+              <label for="inputEmail">Email address</label>
+              <input type="email" class="form-control" id="inputEmail">
+            </div>
+            <div class="form-group">
+              <label for="inputUserName">User Name</label>
+              <input type="text" class="form-control" id="inputUserName">
+            </div>
+            <div class="form-group">
+              <label for="inputPassword">Enter password</label>
+              <input type="password" class="form-control" id="inputPassword">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php
+    require("partials/footer.php");
+    ?>
